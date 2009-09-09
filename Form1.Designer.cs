796c1eddcfdf86ed1c0e516b0d32e7910916b8ee
matuck <matuck@d440e9e7-71d7-43b6-aab1-8ed9c9af1b73>
2009-09-09 14:46:43 +0000

    worked more on the layout and button designs.  adding images for buttons
    git-svn-id: https://zion.shawmfg.shawinc.com/svn/arclaunch/trunk@11 d440e9e7-71d7-43b6-aab1-8ed9c9af1b73

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,21 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arclaunch));
             this.splitlayout = new System.Windows.Forms.SplitContainer();
-            this.serverspnl = new System.Windows.Forms.Panel();
-            this.settingspnl = new System.Windows.Forms.Panel();
-            this.logpnl = new System.Windows.Forms.Panel();
-            this.serversbtn = new System.Windows.Forms.Button();
-            this.settingsbtn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.logsbtn = new System.Windows.Forms.Button();
+            this.settingsbtn = new System.Windows.Forms.Button();
+            this.serversbtn = new System.Windows.Forms.Button();
+            this.serverspnl = new System.Windows.Forms.Panel();
             this.serverslbl = new System.Windows.Forms.Label();
-            this.settingslbl = new System.Windows.Forms.Label();
+            this.logpnl = new System.Windows.Forms.Panel();
             this.logslbl = new System.Windows.Forms.Label();
+            this.settingspnl = new System.Windows.Forms.Panel();
+            this.settingslbl = new System.Windows.Forms.Label();
             this.splitlayout.Panel1.SuspendLayout();
             this.splitlayout.Panel2.SuspendLayout();
             this.splitlayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.serverspnl.SuspendLayout();
-            this.settingspnl.SuspendLayout();
             this.logpnl.SuspendLayout();
+            this.settingspnl.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitlayout
@@ -55,6 +59,10 @@
             // 
             // splitlayout.Panel1
             // 
+            this.splitlayout.Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.splitlayout.Panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("splitlayout.Panel1.BackgroundImage")));
+            this.splitlayout.Panel1.Controls.Add(this.pictureBox2);
+            this.splitlayout.Panel1.Controls.Add(this.pictureBox1);
             this.splitlayout.Panel1.Controls.Add(this.logsbtn);
             this.splitlayout.Panel1.Controls.Add(this.settingsbtn);
             this.splitlayout.Panel1.Controls.Add(this.serversbtn);
@@ -70,6 +78,84 @@
             this.splitlayout.SplitterDistance = 192;
             this.splitlayout.TabIndex = 0;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 127);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(167, 22);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logsbtn
+            // 
+            this.logsbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logsbtn.BackgroundImage")));
+            this.logsbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logsbtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.logsbtn.FlatAppearance.BorderSize = 0;
+            this.logsbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.logsbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.logsbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logsbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.logsbtn.Location = new System.Drawing.Point(12, 98);
+            this.logsbtn.Name = "logsbtn";
+            this.logsbtn.Padding = new System.Windows.Forms.Padding(1);
+            this.logsbtn.Size = new System.Drawing.Size(167, 23);
+            this.logsbtn.TabIndex = 2;
+            this.logsbtn.Text = "Logs";
+            this.logsbtn.UseVisualStyleBackColor = true;
+            this.logsbtn.Click += new System.EventHandler(this.logsbtn_Click);
+            // 
+            // settingsbtn
+            // 
+            this.settingsbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("settingsbtn.BackgroundImage")));
+            this.settingsbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.settingsbtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.settingsbtn.FlatAppearance.BorderSize = 0;
+            this.settingsbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.settingsbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.settingsbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.settingsbtn.Location = new System.Drawing.Point(12, 69);
+            this.settingsbtn.Name = "settingsbtn";
+            this.settingsbtn.Padding = new System.Windows.Forms.Padding(1);
+            this.settingsbtn.Size = new System.Drawing.Size(167, 23);
+            this.settingsbtn.TabIndex = 1;
+            this.settingsbtn.Text = "Settings";
+            this.settingsbtn.UseVisualStyleBackColor = true;
+            this.settingsbtn.Click += new System.EventHandler(this.settingsbtn_Click);
+            // 
+            // serversbtn
+            // 
+            this.serversbtn.BackColor = System.Drawing.Color.Transparent;
+            this.serversbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("serversbtn.BackgroundImage")));
+            this.serversbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.serversbtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.serversbtn.FlatAppearance.BorderSize = 0;
+            this.serversbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.serversbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.serversbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.serversbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.serversbtn.Location = new System.Drawing.Point(12, 40);
+            this.serversbtn.Name = "serversbtn";
+            this.serversbtn.Padding = new System.Windows.Forms.Padding(1);
+            this.serversbtn.Size = new System.Drawing.Size(167, 23);
+            this.serversbtn.TabIndex = 0;
+            this.serversbtn.Text = "Servers";
+            this.serversbtn.UseVisualStyleBackColor = true;
+            this.serversbtn.Click += new System.EventHandler(this.serversbtn_Click);
+            // 
             // serverspnl
             // 
             this.serverspnl.BackColor = System.Drawing.Color.Transparent;
@@ -79,56 +165,6 @@
             this.serverspnl.Name = "serverspnl";
             this.serverspnl.Size = new System.Drawing.Size(474, 376);
             this.serverspnl.TabIndex = 0;
-            // 
-            // settingspnl
-            // 
-            this.settingspnl.BackColor = System.Drawing.Color.Transparent;
-            this.settingspnl.Controls.Add(this.settingslbl);
-            this.settingspnl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.settingspnl.Location = new System.Drawing.Point(0, 0);
-            this.settingspnl.Name = "settingspnl";
-            this.settingspnl.Size = new System.Drawing.Size(474, 376);
-            this.settingspnl.TabIndex = 0;
-            // 
-            // logpnl
-            // 
-            this.logpnl.BackColor = System.Drawing.Color.Transparent;
-            this.logpnl.Controls.Add(this.logslbl);
-            this.logpnl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.logpnl.Location = new System.Drawing.Point(0, 0);
-            this.logpnl.Name = "logpnl";
-            this.logpnl.Size = new System.Drawing.Size(474, 376);
-            this.logpnl.TabIndex = 0;
-            // 
-            // serversbtn
-            // 
-            this.serversbtn.Location = new System.Drawing.Point(12, 12);
-            this.serversbtn.Name = "serversbtn";
-            this.serversbtn.Size = new System.Drawing.Size(167, 23);
-            this.serversbtn.TabIndex = 0;
-            this.serversbtn.Text = "Servers";
-            this.serversbtn.UseVisualStyleBackColor = true;
-            this.serversbtn.Click += new System.EventHandler(this.serversbtn_Click);
-            // 
-            // settingsbtn
-            // 
-            this.settingsbtn.Location = new System.Drawing.Point(13, 41);
-            this.settingsbtn.Name = "settingsbtn";
-            this.settingsbtn.Size = new System.Drawing.Size(167, 23);
-            this.settingsbtn.TabIndex = 1;
-            this.settingsbtn.Text = "Settings";
-            this.settingsbtn.UseVisualStyleBackColor = true;
-            this.settingsbtn.Click += new System.EventHandler(this.settingsbtn_Click);
-            // 
-            // logsbtn
-            // 
-            this.logsbtn.Location = new System.Drawing.Point(12, 70);
-            this.logsbtn.Name = "logsbtn";
-            this.logsbtn.Size = new System.Drawing.Size(167, 23);
-            this.logsbtn.TabIndex = 2;
-            this.logsbtn.Text = "Logs";
-            this.logsbtn.UseVisualStyleBackColor = true;
-            this.logsbtn.Click += new System.EventHandler(this.logsbtn_Click);
             // 
             // serverslbl
             // 
@@ -140,15 +176,15 @@
             this.serverslbl.TabIndex = 0;
             this.serverslbl.Text = "Servers";
             // 
-            // settingslbl
+            // logpnl
             // 
-            this.settingslbl.AutoSize = true;
-            this.settingslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.settingslbl.Location = new System.Drawing.Point(12, 12);
-            this.settingslbl.Name = "settingslbl";
-            this.settingslbl.Size = new System.Drawing.Size(90, 25);
-            this.settingslbl.TabIndex = 1;
-            this.settingslbl.Text = "Settings";
+            this.logpnl.BackColor = System.Drawing.Color.Transparent;
+            this.logpnl.Controls.Add(this.logslbl);
+            this.logpnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logpnl.Location = new System.Drawing.Point(0, 0);
+            this.logpnl.Name = "logpnl";
+            this.logpnl.Size = new System.Drawing.Size(474, 376);
+            this.logpnl.TabIndex = 0;
             // 
             // logslbl
             // 
@@ -159,6 +195,26 @@
             this.logslbl.Size = new System.Drawing.Size(59, 25);
             this.logslbl.TabIndex = 1;
             this.logslbl.Text = "Logs";
+            // 
+            // settingspnl
+            // 
+            this.settingspnl.BackColor = System.Drawing.Color.Transparent;
+            this.settingspnl.Controls.Add(this.settingslbl);
+            this.settingspnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.settingspnl.Location = new System.Drawing.Point(0, 0);
+            this.settingspnl.Name = "settingspnl";
+            this.settingspnl.Size = new System.Drawing.Size(474, 376);
+            this.settingspnl.TabIndex = 0;
+            // 
+            // settingslbl
+            // 
+            this.settingslbl.AutoSize = true;
+            this.settingslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingslbl.Location = new System.Drawing.Point(12, 12);
+            this.settingslbl.Name = "settingslbl";
+            this.settingslbl.Size = new System.Drawing.Size(90, 25);
+            this.settingslbl.TabIndex = 1;
+            this.settingslbl.Text = "Settings";
             // 
             // Arclaunch
             // 
@@ -177,12 +233,14 @@
             this.splitlayout.Panel1.ResumeLayout(false);
             this.splitlayout.Panel2.ResumeLayout(false);
             this.splitlayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.serverspnl.ResumeLayout(false);
             this.serverspnl.PerformLayout();
-            this.settingspnl.ResumeLayout(false);
-            this.settingspnl.PerformLayout();
             this.logpnl.ResumeLayout(false);
             this.logpnl.PerformLayout();
+            this.settingspnl.ResumeLayout(false);
+            this.settingspnl.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -199,6 +257,8 @@
         private System.Windows.Forms.Label serverslbl;
         private System.Windows.Forms.Label logslbl;
         private System.Windows.Forms.Label settingslbl;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
