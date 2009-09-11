c1c87f6cfb74dc4308d39dce7c3fc2cdfee56dbe
matuck <matuck@d440e9e7-71d7-43b6-aab1-8ed9c9af1b73>
2009-09-11 03:53:12 +0000

    forgot to add a few files on last commit.
    git-svn-id: https://zion.shawmfg.shawinc.com/svn/arclaunch/trunk@18 d440e9e7-71d7-43b6-aab1-8ed9c9af1b73

---
+++ addsrv.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Arclaunch
+{
+    partial class addsrv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addsrv));
+            this.SuspendLayout();
+            // 
+            // addsrv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "addsrv";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Add World Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
