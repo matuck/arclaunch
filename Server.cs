6d1fd66bdb98339da43fb133dcbbfd00e5136fc1
matuck <matuck@d440e9e7-71d7-43b6-aab1-8ed9c9af1b73>
2009-09-21 23:09:36 +0000

    Server.cs needs to be added in  some how was left off previous update.
    git-svn-id: https://zion.shawmfg.shawinc.com/svn/arclaunch/trunk@36 d440e9e7-71d7-43b6-aab1-8ed9c9af1b73

---
+++ Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arclaunch
+{
+    class Server
+    {
+        public string type, name, path;
+        public int pid, window;
+    }
+}
